faaaed3665e1523ee3daa760c0bb3a7283cfee8d
Akshudhana <akshathad998@gmail.com>
2024-03-07 11:33:21 +0530

    1st commit

---
+++ src/test/java/gitHubMavenProjetPractice/MavenSample.java
@@ -0,0 +1,14 @@
+package gitHubMavenProjetPractice;
+
+public class MavenSample {
+
+	public static void main(String[] args) {
+		System.out.println("--mavenSample1--");
+	}
+	
+	
+	
+	public void mavenSample() {
+		System.out.println("--mavenSample2--");
+	}
+}
